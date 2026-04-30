310645a4a73cf59f25134032577c811ce711f79b
xStaked <aljndr911@gmail.com>
2026-04-30 09:35:45 -0500

    fix: import AuthModule in DashboardModule

--- apps/api/src/modules/dashboard/dashboard.module.ts
+++ apps/api/src/modules/dashboard/dashboard.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
 import { PrismaModule } from "../../prisma/prisma.module";
 import { DashboardController } from "./dashboard.controller";
 import { DashboardService } from "./dashboard.service";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
